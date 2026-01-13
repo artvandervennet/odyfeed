7a62e7047343a7217e5921dde6224b846cb33c0b
Art Van der Vennet <art.van.der.vennet.be@gmail.com>
2026-01-13 15:48:09 +0100

    feat: add meta tags for character set and viewport
    Includes UTF-8 charset and viewport settings to improve
    responsiveness on mobile devices. This enhances user experience
    by ensuring proper rendering across different screen sizes.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,6 +28,10 @@ export default defineNuxtConfig({
 			htmlAttrs: {
 				lang: 'en'
 			},
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' }
+			],
 			link: [
 				{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
 				{rel: 'me', href: 'https://github.com/artvandervennet'}
